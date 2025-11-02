6d522bd43a94c902eb222cf387af98d09e3a9261
Eugen Hildt <eugen.hildt@gmail.com>
2025-11-02 13:27:25 +0100

    0012: updated socket-io constants

--- apps/visions/src/main.module.ts
+++ apps/visions/src/main.module.ts
@@ -1,12 +1,13 @@
+import { BULLMQ_QUEUE, BullMQModule } from '@ehildt/ckir-bullmq';
 import { ConfigFactoryModule } from '@ehildt/ckir-config-factory';
 import { OllamaModule } from '@ehildt/ckir-ollama';
-import { QdrantModule } from '@ehildt/ckir-qdrant';
+import { SocketIOModule } from '@ehildt/ckir-socket-io';
 import { Logger, Module } from '@nestjs/common';
 
 import { AppConfigService } from './configs/app-config.service';
 import { BullMQConfigService } from './configs/bullmq-config.service';
 import { OllamaConfigService } from './configs/ollama-config.service';
-import { QdrantConfigService } from './configs/qdrant-config.service';
+import { SocketIOConfigService } from './configs/socket-io-config.service';
 import { ImagesController } from './controllers/images.controller';
 import { VectorsService } from './services/vectors.service';
 
@@ -20,7 +21,7 @@ import { VectorsService } from './services/vectors.service';
         AppConfigService,
         BullMQConfigService,
         OllamaConfigService,
-        QdrantConfigService,
+        SocketIOConfigService,
       ],
     }),
     OllamaModule.registerAsync({
@@ -30,24 +31,25 @@ import { VectorsService } from './services/vectors.service';
         host: xOllamaConfig.host,
       }),
     }),
-    QdrantModule.registerAsync({
+    BullMQModule.registerAsync({
       global: true,
-      inject: [QdrantConfigService],
-      useFactory: async ({ qdrantConfig }: QdrantConfigService) => qdrantConfig,
+      inject: [BullMQConfigService],
+      queues: [
+        BULLMQ_QUEUE.VISIONS_OCR,
+        BULLMQ_QUEUE.VISIONS_COMPARE,
+        BULLMQ_QUEUE.VISIONS_DESCRIBE,
+      ],
+      processors: [],
+      usePinoFactory: async ({ pinoConfig }: BullMQConfigService) => pinoConfig,
+      useBullFactory: async ({ bullMQConfig }: BullMQConfigService) =>
+        bullMQConfig,
     }),
-    // BullMQModule.registerAsync({
-    //   global: true,
-    //   inject: [BullMQConfigService],
-    //   queues: [
-    //     BULLMQ_QUEUE.VECTORIZE_POST,
-    //     BULLMQ_QUEUE.VECTORIZE_THREAD,
-    //     BULLMQ_QUEUE.VECTORIZE_TOPIC,
-    //   ],
-    //   processors: [PostsProcessor, ThreadsProcessor, TopicsProcessor],
-    //   usePinoFactory: async ({ pinoConfig }: BullMQConfigService) => pinoConfig,
-    //   useBullFactory: async ({ bullMQConfig }: BullMQConfigService) =>
-    //     bullMQConfig,
-    // }),
+    SocketIOModule.registerAsync({
+      global: true,
+      inject: [SocketIOConfigService],
+      useFactory: async ({ socketIOConfig }: SocketIOConfigService) =>
+        socketIOConfig,
+    }),
   ],
 })
 export class MainModule {}
